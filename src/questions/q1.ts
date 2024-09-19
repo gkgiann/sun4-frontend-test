0c46ac566d0c294f44bf0141626897baed26cc6e
gian <gian@pop-os.localdomain>
2024-09-18 22:31:11 -0300

    feat: add question 01

---
+++ src/questions/q1.ts
@@ -0,0 +1,29 @@
+export function q1RemoverDuplicados(nums: number[]) {
+  /**
+   * Retorna uma lista com apenas um registro de cada elemento
+   *
+   * Saída esperada: [1, 2, 3, 4, 5]
+   */
+
+  let numbersWithoutDuplicates: number[] = [];
+
+  nums.forEach((number, numberIndex) => {
+    if (!numbersWithoutDuplicates.includes(number)) {
+      numbersWithoutDuplicates.push(number);
+    } else {
+      nums.splice(numberIndex, 1);
+    }
+  });
+
+  return nums;
+}
+
+// console.log(q1RemoverDuplicados([1, 2, 2, 3, 4, 4, 5]));
+
+export function q1RemoverDuplicadosUtilizandoFilter(nums: number[]) {
+  const numbersWithoutDuplicates = nums.filter((number, numberIndex) => {
+    return nums.indexOf(number) === numberIndex;
+  });
+
+  return numbersWithoutDuplicates;
+}
