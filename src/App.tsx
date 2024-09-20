a837f1b4ca5d8154a173a451db0895a29a14f170
gian <gian@pop-os.localdomain>
2024-09-20 18:04:45 -0300

    feat: show question results

--- src/App.tsx
+++ src/App.tsx
@@ -10,14 +10,10 @@ import { q2ContarFrequenciaPalavra } from "./questions/q2";
 import { q3TratarDatas } from "./questions/q3";
 
 function App() {
-  console.log("======== Questão 01 =========");
-  console.log(q1RemoverDuplicados([1, 2, 2, 3, 4, 4, 5]));
-  console.log(q1RemoverDuplicadosUtilizandoFilter([1, 2, 2, 3, 4, 4, 5]));
+  const q1 = q1RemoverDuplicados([1, 2, 2, 3, 4, 4, 5]);
+  const q1Filter = q1RemoverDuplicadosUtilizandoFilter([1, 2, 2, 3, 4, 4, 5]);
 
-  console.log("======== Questão 02 =========");
-  console.log(q2ContarFrequenciaPalavra("Hello world hello"));
-
-  console.log("======== Questão 03 =========");
+  const q2 = q2ContarFrequenciaPalavra("Hello world hello");
 
   const datasParaTratar = [
     "30/11/2022",
@@ -26,12 +22,32 @@ function App() {
     "31 de dezembro de 2022",
   ];
 
-  datasParaTratar.forEach((data) => {
-    console.log(q3TratarDatas(data));
-  });
+  return (
+    <div className="flex flex-col gap-4 justify-center items-start p-8 text-xl">
+      <div className="flex gap-2">
+        <span className="font-bold">Questão 01: </span>
+        <span>
+          {JSON.stringify(q1)} <strong>| usando .filter {"=>"} </strong>
+          {JSON.stringify(q1Filter)}
+        </span>
+      </div>
+      <div className="flex gap-2">
+        <span className="font-bold">Questão 02: </span>
+        <span>{JSON.stringify(q2)}</span>
+      </div>
+      <div className="flex gap-2">
+        <span className="font-bold">Questão 03: </span>
+        {datasParaTratar.map((data) => {
+          const dataFormatada = q3TratarDatas(data);
+
+          return <span>{JSON.stringify(dataFormatada)}, </span>;
+        })}
+      </div>
+    </div>
+  );
 
   // return <Question04 />;
-  return <Question05 />;
+  // return <Question05 />;
 }
 
 export default App;
