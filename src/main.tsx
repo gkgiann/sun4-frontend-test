576e5b1e703f718701a35127a9bfc45788c5cc51
gian <gian@pop-os.localdomain>
2024-09-20 18:13:39 -0300

    feat: react router dom setup

--- src/main.tsx
+++ src/main.tsx
@@ -3,8 +3,27 @@ import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
 
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Question04 from "./components/Question04.tsx";
+import Question05 from "./components/Question05.tsx";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+  },
+  {
+    path: "q4",
+    element: <Question04 />,
+  },
+  {
+    path: "q5",
+    element: <Question05 />,
+  },
+]);
+
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </StrictMode>
 );
